6a1febc15cb05eec6351a6d69a45c8b37dd55b54
butterrzz <38452206+butterrzz@users.noreply.github.com>
2018-10-16 23:54:07 -0400

    got the ball rolling...

--- client/src/component/home/home.jsx
+++ client/src/component/home/home.jsx
@@ -3,7 +3,13 @@ import { Link } from 'react-router-dom';
 import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
 import './home.css';
 
+
+
 export default class Home extends Component {
+    constructor() {
+        super()
+    }
+    
     render() {
         return (
             <Grid>
